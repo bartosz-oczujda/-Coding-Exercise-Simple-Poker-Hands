adf0bdc391fcd873e3d7af9357ff7b60fc3a9ff6
Bartosz Oczujda <bart@Bartoszs-Mac.local>
2016-01-31 12:42:08 +0100

    Initial Xcode project files

---
+++ Simple-Poker-Hands/Simple-Poker-Hands/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Simple-Poker-Hands
+//
+//  Created by Bartosz Oczujda on 31/01/16.
+//  Copyright © 2016 Pixelrockit. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
